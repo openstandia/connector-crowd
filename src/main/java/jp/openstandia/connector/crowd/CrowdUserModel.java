50e0a0601baaf9c44ecd92bfe914335bc1b8ebcc
Hiroyuki Wada <h2-wada@nri.co.jp>
2022-12-28 17:05:43 +0900

    fix: remove group from user

--- src/main/java/jp/openstandia/connector/crowd/CrowdUserModel.java
+++ src/main/java/jp/openstandia/connector/crowd/CrowdUserModel.java
@@ -170,6 +170,6 @@ public class CrowdUserModel {
         if (this.removeGroups == null) {
             this.removeGroups = new ArrayList<>();
         }
-        this.addGroups.addAll(groups);
+        this.removeGroups.addAll(groups);
     }
 }
