5554087b6bba931de4152183c82d98667a881525
Hiroyuki Wada <h2-wada@nri.co.jp>
2022-12-28 17:52:51 +0900

    chore: add license

--- src/main/java/jp/openstandia/connector/util/ObjectHandler.java
+++ src/main/java/jp/openstandia/connector/util/ObjectHandler.java
@@ -20,7 +20,7 @@ import org.identityconnectors.framework.common.objects.*;
 import java.util.Set;
 
 /**
- * Provides utility methods
+ * Define handler methods for connector operations.
  *
  * @author Hiroyuki Wada
  */
